1d143b8b1daa7b8b47a9f02a4c3e58654a792cc2
yanotyan <m.parfume.y.ha7@gmail.com>
2021-02-26 12:17:33 +0900

    create　アクション記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,8 +5,23 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+
     @tag = Tag.all
   end
 
+  def create
+    @post = Post.new(post_params)
+    binding.pry
+    if @post.save
+      redirect_to root_path, notice: '投稿しました'
+    else
+      render :new, notice: 'もう一度入力してください'
+    end
 
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:name, :photo, :content, tag_ids: []).merge(user_id: current_user.id)
+  end
 end
